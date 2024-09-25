23c1fbcc241c9c4eeaeffc00307834e329587edd
Khaled <hy3113194@gmail.com>
2024-09-25 04:55:33 +0300

    hot fix #2

--- lib/core/helpers/validations.dart
+++ lib/core/helpers/validations.dart
@@ -3,55 +3,55 @@ import 'package:warsha/core/helpers/app_regex.dart';
 class Validations {
   static String? userNameValidator(String? value) {
     if (value == null || value.isEmpty) {
-      return "Username cannot be empty";
+      return "Username cannot be empty.";
     }
     if (value.length <= 2) {
-      return "Username must be at least 3 characters long";
+      return "Username should be at least 3 characters long.";
     }
     if (value.length > 40) {
-      return "Username cannot be more than 40 characters long";
+      return "Username cannot exceed 40 characters.";
     }
     if (!AppRegex.isUsernameValid(value)) {
-      return "Please enter a valid username";
+      return "Please enter a valid username.";
     }
     return null;
   }
 
   static String? emailValidator(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Email cannot be empty!';
+      return 'Email address is required.';
     }
     if (!AppRegex.isEmailValid(value)) {
-      return 'Please enter valid email';
+      return 'Please enter a valid email address.';
     }
     return null;
   }
 
   static String? phoneNumberValidator(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Phone Number cannot be empty!';
+      return 'Phone number is required.';
     }
     if (!AppRegex.isPhoneNumberValid(value)) {
-      return 'Please enter valid Phone Number';
+      return 'Please enter a valid phone number.';
     }
     return null;
   }
 
   static String? passwordValidator(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Password cannot be empty!';
+      return 'Password is required.';
     }
     if (value.length < 8) {
-      return 'Password must be at least 8 chars long!';
+      return 'Password should be at least 8 characters long.';
     }
     if (!AppRegex.hasUpperCase(value)) {
-      return 'Password must include an uppercase letter!';
+      return 'Password must contain at least one uppercase letter.';
     }
     if (!AppRegex.hasLowerCase(value)) {
-      return 'Password must include a lowercase letter!';
+      return 'Password must contain at least one lowercase letter.';
     }
     if (!AppRegex.hasNumber(value)) {
-      return 'Password must include a number!';
+      return 'Password must contain at least one number.';
     }
     return null;
   }
