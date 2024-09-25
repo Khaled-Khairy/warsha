dcef3c0b92611ed55ed08123bfcc0b1523232135
Khaled <hy3113194@gmail.com>
2024-09-25 22:57:14 +0300

    adding animation

--- lib/core/widgets/courses_list.dart
+++ lib/core/widgets/courses_list.dart
@@ -1,4 +1,5 @@
 import 'package:warsha/core/helpers/common_imports.dart';
+import 'package:warsha/core/widgets/animate_list.dart';
 
 class CoursesList extends StatefulWidget {
   const CoursesList({super.key, required this.courses, this.onTap});
@@ -11,6 +12,18 @@ class CoursesList extends StatefulWidget {
 }
 
 class _CoursesListState extends State<CoursesList> {
+  bool myAnimation = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      setState(() {
+        myAnimation = true;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -19,17 +32,23 @@ class _CoursesListState extends State<CoursesList> {
       padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
       physics: const BouncingScrollPhysics(),
       itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.only(bottom: 12.h),
-          child: GestureDetector(
-            onTap: widget.onTap != null ? () => widget.onTap!(index) : () {
-                  context.pushNamed(
-                    Routes.courseDetailsScreen,
-                    arguments: widget.courses[index],
-                  );
-                },
-            child: CourseContentCard(
-              course: widget.courses[index],
+        return AnimateList(
+          myAnimation: myAnimation,
+          index: index,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 12.h),
+            child: GestureDetector(
+              onTap: widget.onTap != null
+                  ? () => widget.onTap!(index)
+                  : () {
+                      context.pushNamed(
+                        Routes.courseDetailsScreen,
+                        arguments: widget.courses[index],
+                      );
+                    },
+              child: CourseContentCard(
+                course: widget.courses[index],
+              ),
             ),
           ),
         );
--- lib/features/categories/presentation/views/widgets/category_courses_list.dart
+++ lib/features/categories/presentation/views/widgets/category_courses_list.dart
@@ -1,4 +1,5 @@
 import 'package:warsha/core/helpers/common_imports.dart';
+import 'package:warsha/core/widgets/courses_list.dart';
 
 class CategoryCoursesList extends StatelessWidget {
   const CategoryCoursesList({
@@ -11,27 +12,7 @@ class CategoryCoursesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView.builder(
-        itemCount: courses.length,
-        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(bottom: 12.h),
-            child: GestureDetector(
-              onTap: () {
-                context.pushNamed(
-                  Routes.courseDetailsScreen,
-                  arguments: courses[index],
-                );
-              },
-              child: CourseContentCard(
-                course: courses[index],
-              ),
-            ),
-          );
-        },
-      ),
+      child: CoursesList(courses: courses),
     );
   }
 }
