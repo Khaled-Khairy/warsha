59da9139654d099a91248ed069797d1a18b60f31
Khaled <hy3113194@gmail.com>
2024-09-27 00:10:29 +0300

    check for course state ui

--- lib/features/my_courses/presentation/views/widget/course_unit_body.dart
+++ lib/features/my_courses/presentation/views/widget/course_unit_body.dart
@@ -1,4 +1,5 @@
 import 'package:warsha/core/helpers/common_imports.dart';
+import 'package:warsha/features/my_courses/presentation/views/widget/course_status.dart';
 
 import 'course_unit_bloc_builder.dart';
 
@@ -19,9 +20,17 @@ class CourseUnitBody extends StatelessWidget {
           );
         } else if (state is SubscriptionStateSuccess) {
           if (state.subscriptionStatus.state == "under_review") {
-            return const Text("under_review");
+            return const CourseStatus(
+              lottie: "assets/lotties/under_review.json",
+              state: "Under Review",
+              message: "Thank you for your purchase! We’re reviewing your request and will notify you once the process is complete. \nWe appreciate your patience.",
+            );
           } else if (state.subscriptionStatus.state == "rejected") {
-            return const Text("rejected");
+            return CourseStatus(
+              lottie: "assets/lotties/rejected.json",
+              state: "Rejected",
+              message: state.subscriptionStatus.reason,
+            );
           } else if (state.subscriptionStatus.state == "confirmed") {
             return CourseUnitBlocBuilder(
               slug: slug,
